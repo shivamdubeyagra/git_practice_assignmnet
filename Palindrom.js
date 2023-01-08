e2ba3c42517db94e888d76f4ae3e652c5d4fda31
shivamdubeyagra <shivamdubeyagra@gmail.com>
2023-01-08 22:19:46 +0530

    one is done

---
+++ Palindrom.js
@@ -0,0 +1,11 @@
+let str='aman';
+let bag='';
+for(let i=str.length-1; i>=0; i++){
+	bag+=str[i];
+}
+if(str==bag){
+		console.log("Palindrom");
+	}else{
+		console.log("Not Palindrom");
+	}
+	
---
+++ prime_number.js
@@ -0,0 +1,12 @@
+let num=3;
+let count=0;
+for(let i=1; i<num; i++){
+	if(num%i==0){
+		count++;
+	}
+if(count==2){
+console.("Prime");
+}
+else{
+console.log("Not Prime");
+}
